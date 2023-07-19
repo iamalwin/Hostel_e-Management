842dace3cd6372879b90a78ce96a24eb2421ae97
iamalwin <139733521+iamalwin@users.noreply.github.com>
2023-07-19 19:54:47 +0530

    index update

--- index.php
+++ index.php
@@ -52,22 +52,22 @@
 
 <body>
     <div class="main-wrapper">
-        <!-- ============================================================== -->
+     
         <!-- Preloader - style you can find in spinners.css -->
-        <!-- ============================================================== -->
+     
         <div class="preloader">
             <div class="lds-ripple">
                 <div class="lds-pos"></div>
                 <div class="lds-pos"></div>
             </div>
         </div>
-        <!-- ============================================================== -->
+     
         <!-- Preloader - style you can find in spinners.css -->
-        <!-- ============================================================== -->
+     
 
-        <!-- ============================================================== -->
+     
         <!-- Login box.scss -->
-        <!-- ============================================================== -->
+     
         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
             style="background:url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
             <div class="auth-box row">
@@ -108,20 +108,20 @@
                 </div>
             </div>
         </div>
-        <!-- ============================================================== -->
+     
         <!-- Login box.scss -->
-        <!-- ============================================================== -->
+     
     </div>
-    <!-- ============================================================== -->
+ 
     <!-- All Required js -->
-    <!-- ============================================================== -->
+ 
     <script src="../assets/libs/jquery/dist/jquery.min.js "></script>
     <!-- Bootstrap tether Core JavaScript -->
     <script src="../assets/libs/popper.js/dist/umd/popper.min.js "></script>
     <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js "></script>
-    <!-- ============================================================== -->
+ 
     <!-- This page plugin js -->
-    <!-- ============================================================== -->
+ 
     <script>
         $(".preloader ").fadeOut();
     </script>
