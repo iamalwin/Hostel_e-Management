13f82ed7348d029fbf90894d2dc96db4611c8433
iamalwin <139733521+iamalwin@users.noreply.github.com>
2023-07-25 18:54:12 +0530

    stud update

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -46,7 +46,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="../feedback.php">
+            <a class="nav-link" href="../get_feedback.php">
                 <span class="menu-title">Feedback</span>
                 <i class="mdi mdi-chart-bar menu-icon"></i>
             </a>
